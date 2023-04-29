491c394d80d1e9c2d9b7bb244154f417b888ffe6
Gregory Dutton <greg.dutton@gmail.com>
2023-04-29 16:22:49 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+let num1 = 2, num2 = 31, num3 = 20, num4 = 8;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+let mod = num3 % num4;
+
+let max = Math.max(2, 4, 6, 8, 10, 20);
